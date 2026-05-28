66cf1156dff84200ad3bee4a5e302e73c4171a25
anamikaaa12 <anamikasri76350@gmail.com>
2026-05-28 22:15:21 +0530

    DAY-1 uploaded

---
+++ DAY-1/multiplicationtable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class multiplicationtable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+        System.out.println("Multiplication Table of " + n + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+        sc.close();
+    }
+}
