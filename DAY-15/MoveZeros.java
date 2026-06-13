c04187e6c50c292ef4b721968fca4ddeb77a5252
anamikaaa12 <anamikasri76350@gmail.com>
2026-06-13 22:39:47 +0530

    uploaded DAY-15

---
+++ DAY-15/MoveZeros.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+        int arr[] = {1, 0, 3, 0, 5, 0, 7};
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
